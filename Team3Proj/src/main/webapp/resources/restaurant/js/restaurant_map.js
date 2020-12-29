3aff7222c8876ce2ff5c382e88f67ad327689384
LDK <burgerpla96@gmail.com>
2020-12-29 13:27:43 +0900

    201229-LDK 지도 카테고리화 추가

--- Team3Proj/src/main/webapp/resources/restaurant/js/restaurant_map.js
+++ Team3Proj/src/main/webapp/resources/restaurant/js/restaurant_map.js
@@ -1,60 +1,21 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<%@ taglib prefix="sec"
-	uri="http://www.springframework.org/security/tags"%>
+// 마커이미지의 주소와, 크기, 옵션으로 마커 이미지를 생성하여 리턴하는 함수입니다
+function createMarkerImage(src, size, options) {
+	var markerImage = new kakao.maps.MarkerImage(src, size,
+			options);
+	return markerImage;
+}
 
-<section id="home" class="home bg-black fix">
-	<div class="container">
-		<div class="row">
-			<div class="main_home text-center">
-				<div class="col-md-12">
-					<div class="hello">
-						<div class="slid_item">
-							<div class="home_text ">
-								<h1 class="text-yellow">Find Restaurant</h1>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<!-- main_home -->
-		</div>
-		<!-- row -->
-	</div>
-	<!-- container -->
-</section>
+// 좌표와 마커이미지를 받아 마커를 생성하여 리턴하는 함수입니다
+function createMarker(position, image) {
+	var marker = new kakao.maps.Marker({
+		position : position,
+		image : image
+	});
+	return marker;
+}
 
 
 
-<!-- /veve/resources/restaurant/images/restaurant.png -->
-<!-- 지도 css 수정 필요 -->
-<link rel="stylesheet"
-	href="/veve/resources/restaurant/css/restaurant_map.css">
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-			<p></p>
-		</div>
-		<div class="col-md-12">
-			<p></p>
-		</div>
-	</div>
-</div>
-
-
-
-<div class="container">
-	<div class="row">
-		<div class="col-md-12">
-
-			<div id="mapwrap">
-				<!-- 지도가 표시될 div -->
-				<div id="map" style="width: 100%; height: 600px;"></div>
-			</div>
-
-			<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d0c2399991c756eb5abacf77c945032a&libraries=services,clusterer"></script>
-			<script>
 				var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
 				mapOption = {
 					center : new kakao.maps.LatLng(37.498004414546934,
@@ -131,104 +92,60 @@
 
 				
 				
-				
-				
-				
-				//////////////////////////////////////////////////로직
-				
-				
-				// 마커이미지의 주소와, 크기, 옵션으로 마커 이미지를 생성하여 리턴하는 함수입니다
-				function createMarkerImage(src, size, options) {
-					var markerImage = new kakao.maps.MarkerImage(src, size,
-							options);
-					return markerImage;
-				}
-
-				// 좌표와 마커이미지를 받아 마커를 생성하여 리턴하는 함수입니다
-				function createMarker(position, image) {
-					var marker = new kakao.maps.Marker({
-						position : position,
-						image : image
-					});
-					return marker;
-				}
-				
 				var overlay = null;
-				
 				
 				// 커피숍 마커를 생성하고 커피숍 마커 배열에 추가하는 함수입니다
 				function createCoffeeMarkers() {
 
-					var coffeeMarkerName = [];
-					
 					for (var i = 0; i < coffeePositions.length; i++) {
 
-						var imageSize = new kakao.maps.Size(40, 45), imageOptions = {
+						var imageSize = new kakao.maps.Size(22, 26), imageOptions = {
 							offset : new kakao.maps.Point(27, 69)
 						};
 
 						// 마커이미지와 마커를 생성합니다
 						var markerImage = createMarkerImage(
-								markerRestaurantImage, imageSize, imageOptions);
-						eval("var marker"+i+"= createMarker(coffeePositions[i], markerImage);");
+								markerRestaurantImage, imageSize, imageOptions), 
+								marker = createMarker(
+								coffeePositions[i], markerImage);
 						
-	
-					}
-					/////////////////////////////오버레이 
-					//오버레이설정
-					var content = '<div class="wrap">'
-							+ '    <div class="info">'
-							+ '        <div class="title">'
-							+ '            가산동 서브웨이'
-							+ '            <div class="close" onclick="closeOverlay()" title="닫기"></div>'
-							+ '        </div>'
-							+ '        <div class="body">'
-							+ '            <div class="img">'
-							+ '                <img src="/veve/resources/restaurant/images/subway.png" width="73" height="70">'
-							+ '           </div>'
-							+ '            <div class="desc">'
-							+ '                <div class="ellipsis">간단 설명</div>'
-							+ '                <div class="jibun ellipsis">주소적기</div>'
-							+ '                <div><a href="https://www.subway.co.kr/" target="_blank" class="link">식당 리뷰보기</a></div>'
-							+ '            </div>' + '        </div>'
-							+ '    </div>' + '</div>';
-	
-					// 마커 위에 커스텀오버레이를 표시합니다
-					// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
-					overlay = new kakao.maps.CustomOverlay({
+						
+						
+						//오버레이설정
+						var content = '<div class="wrap">'
+								+ '    <div class="info">'
+								+ '        <div class="title">'
+								+ '            가산동 서브웨이'
+								+ '            <div class="close" onclick="closeOverlay()" title="닫기"></div>'
+								+ '        </div>'
+								+ '        <div class="body">'
+								+ '            <div class="img">'
+								+ '                <img src="/veve/resources/restaurant/images/subway.png" width="73" height="70">'
+								+ '           </div>'
+								+ '            <div class="desc">'
+								+ '                <div class="ellipsis">간단 설명</div>'
+								+ '                <div class="jibun ellipsis">주소적기</div>'
+								+ '                <div><a href="https://www.subway.co.kr/" target="_blank" class="link">식당 리뷰보기</a></div>'
+								+ '            </div>' + '        </div>'
+								+ '    </div>' + '</div>';
+
+						// 마커 위에 커스텀오버레이를 표시합니다
+						// 마커를 중심으로 커스텀 오버레이를 표시하기위해 CSS를 이용해 위치를 설정했습니다
+						 overlay = new kakao.maps.CustomOverlay({
 							content : content,
 							map : map,
-							position : marker0.getPosition()
-					});
-					/////////////////////////////오버레이 
-					
-					// 생성된 마커를 커피숍 마커 배열에 추가합니다
-					coffeeMarkers.push(marker0);
-					coffeeMarkers.push(marker1);
-					coffeeMarkers.push(marker2);
-					coffeeMarkers.push(marker3);
-					coffeeMarkers.push(marker4);
-					coffeeMarkers.push(marker5);
-					coffeeMarkers.push(marker6);
-				}////////createCoffeeMarkers
-				
-				console.log(typeof coffeeMarkers);
-/////////////////////////////오버레이 
-				//마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-				
-				
-				kakao.maps.event.addListener(coffeeMarkers[0], 'click', function() {
-					overlay.setMap(map);
-					console.log("마커 누름 확인")
-					
-				});
+							position : marker.getPosition()
+						});
+						
+						
+						// 생성된 마커를 커피숍 마커 배열에 추가합니다
+						coffeeMarkers.push(marker);
+	
+					}
 
-				// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
-				function closeOverlay() {
-					overlay.setMap(null);
 				}
-/////////////////////////////오버레이 
 
+				
 
 				// 커피숍 마커들의 지도 표시 여부를 설정하는 함수입니다
 				function setCoffeeMarkers(map) {
@@ -236,21 +153,17 @@
 						coffeeMarkers[i].setMap(map);
 					}
 				}
-				
-				
-
 
 				// 편의점 마커를 생성하고 편의점 마커 배열에 추가하는 함수입니다
 				function createStoreMarkers() {
 					for (var i = 0; i < storePositions.length; i++) {
 
-						var imageSize = new kakao.maps.Size(40, 45), 
-						imageOptions = {
+						var imageSize = new kakao.maps.Size(22, 26), imageOptions = {
 							offset : new kakao.maps.Point(27, 69)
 						};
 
 						// 마커이미지와 마커를 생성합니다
-						var markerImage = createMarkerImage(markerCafeImage,
+						var markerImage = createMarkerImage(markerImageSrc,
 								imageSize, imageOptions), marker = createMarker(
 								storePositions[i], markerImage);
 
@@ -270,13 +183,13 @@
 				function createCarparkMarkers() {
 					for (var i = 0; i < carparkPositions.length; i++) {
 
-						var imageSize = new kakao.maps.Size(40, 45), imageOptions = {
+						var imageSize = new kakao.maps.Size(22, 26), imageOptions = {
 							spriteOrigin : new kakao.maps.Point(10, 72),
 							spriteSize : new kakao.maps.Size(36, 98)
 						};
 
 						// 마커이미지와 마커를 생성합니다
-						var markerImage = createMarkerImage(markerCafeImage,
+						var markerImage = createMarkerImage(markerImageSrc,
 								imageSize, imageOptions), marker = createMarker(
 								carparkPositions[i], markerImage);
 
@@ -287,7 +200,9 @@
 
 				// 주차장 마커들의 지도 표시 여부를 설정하는 함수입니다
 				function setCarparkMarkers(map) {
-					
+					for (var i = 0; i < carparkMarkers.length; i++) {
+						carparkMarkers[i].setMap(map);
+					}
 				}
 
 				// 카테고리를 클릭했을 때 type에 따라 카테고리의 스타일과 지도에 표시되는 마커를 변경합니다
@@ -332,56 +247,19 @@
 						carparkMenu.className = 'menu_selected';
 
 						// 주차장 마커들만 지도에 표시하도록 설정합니다
-						setCoffeeMarkers(map);
-						setStoreMarkers(map);
-						setCarparkMarkers(null);
+						setCoffeeMarkers(null);
+						setStoreMarkers(null);
+						setCarparkMarkers(map);
 					}
 				}
 
-			</script>
-			
-			<script>
-			console.log(coffeeMarkers)
-			
-			console.log(typeof coffeeMarkers);
-			window.onload=function(){ changeMarker('coffee');} // 지도에 커피숍 마커가 보이도록 설정합니다    
-
-			
-				/////////////////////////////오버레이 
 				//마커를 클릭했을 때 커스텀 오버레이를 표시합니다
-				kakao.maps.event.addListener(coffeeMarkers[0], 'click', function() {
+				kakao.maps.event.addListener(marker, 'click', function() {
 					overlay.setMap(map);
 					console.log("마커 누름 확인")
-					
 				});
-				// 위에서는 적용이 안된다...
-			</script>
-			
-		</div>
-	</div>
-</div>
 
-
-
-<div class="text-center">
-	<div style="display:none">
-		<ul><!-- 없으면 버튼이 적용되지 않음 -->
-			<li id="coffeeMenu" onclick="changeMarker('coffee')">
-				<span class="ico_comm ico_coffee"></span> 음식점</li>
-			<li id="storeMenu" onclick="changeMarker('store')">
-				<span class="ico_comm ico_store"></span> 까페</li>
-			<li id="carparkMenu" onclick="changeMarker('carpark')">
-				<span class="ico_comm ico_carpark"></span> 함께 보기</li> 
-		</ul> 
-	</div>
-	<div style="visibility: hidden"><!-- 여백용 -->
-		<ul> <li></li> </ul>
-	</div>
-	<button type="button" class="btn btn-warning" onclick="changeMarker('coffee')">음식점</button>
-	<button type="button" class="btn btn-warning" onclick="changeMarker('store')">까페</button>
-	<button type="button" class="btn btn-warning" onclick="changeMarker('carpark')">함께 보기</button>
-	<div style="visibility: hidden"><!-- 여백용 -->
-		<ul> <li></li> </ul>
-	</div>
-</div>
-
+				// 커스텀 오버레이를 닫기 위해 호출되는 함수입니다 
+				function closeOverlay() {
+					overlay.setMap(null);
+				}
